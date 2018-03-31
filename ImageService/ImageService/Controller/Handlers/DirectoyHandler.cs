6354e33bd43da83e2b804a7e1095d922550588e5
Omer Dekel <omerdekel92@gmail.com>
2018-03-31 21:31:45 +0300

    start impiement DirectoryHandler , need to continue with startHandle function implement

--- ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -21,9 +21,48 @@ namespace ImageService.Controller.Handlers
         private FileSystemWatcher m_dirWatcher;             // The Watcher of the Dir
         private string m_path;                              // The Path of directory
         #endregion
-
         public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;              // The Event That Notifies that the Directory is being closed
 
-		// Implement Here!
+        public DirectoyHandler(IImageController m_controller, ILoggingService m_logging, FileSystemWatcher m_dirWatcher,
+            string m_path)
+        {
+            this.m_controller = m_controller;
+            this.m_logging = m_logging;
+            this.m_path = m_path;
+            this.m_dirWatcher = new FileSystemWatcher(m_path);
+        }
+
+        public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
+        {
+            bool result;
+            //throw new NotImplementedException();
+            if (m_path.Equals(e.Args[0]))
+            {
+                m_controller.ExecuteCommand(e.CommandID, e.Args, out result);
+
+                // check the result and send to the logger
+            }
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string[] fullPath = { e.FullPath }; 
+            string pathExtension = Path.GetExtension(e.FullPath);
+            if (pathExtension.Equals(".jpg") || pathExtension.Equals(".png") || pathExtension.Equals(".gif") || pathExtension.Equals(".bmp")) {
+                CommandRecievedEventArgs commandRecievedEventArgs = new CommandRecievedEventArgs(0,fullPath , m_path);
+                OnCommandRecieved(this, commandRecievedEventArgs);
+            }
+        }
+
+        public void StartHandleDirectory(string dirPath)
+        {
+     
+            m_dirWatcher.Created += new FileSystemEventHandler(OnCreated);
+            // begin watching .
+            m_dirWatcher.EnableRaisingEvents = true;
+            //throw new NotImplementedException();
+        }
+
+        // Implement Here!
     }
 }
--- ImageService/ImageService/Modal/ImageServiceModal.cs
+++ ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -15,6 +15,7 @@ namespace ImageService.Modal
     public class ImageServiceModal : IImageServiceModal
     {
         #region Members
+        //private logger
         private string m_OutputFolder;            // The Output Folder
         private int m_thumbnailSize;              // The Size Of The Thumbnail Size
         // prototype
