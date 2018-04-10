2dc9a838725645935c526fdf2ea46d73ce6b1eaa
Omer Dekel <omerdekel92@gmail.com>
2018-04-10 21:02:33 +0300

    updating directory handler message to the log and closing handler

--- ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -30,14 +30,19 @@ namespace ImageService.Controller.Handlers
             this.m_logging = m_logging;
             this.m_path = m_path;
             this.m_dirWatcher = new FileSystemWatcher(m_path);
+            m_logging.Log("Created new DirectoryHandler", MessageTypeEnum.INFO);
         }
 
         public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
         {
             bool result;
             // if the command is relevant to our directory
-            if (m_path.Equals(e.Args[0]))
+            if (m_path.Equals(e.Args[0]) || e.Args[0].Equals("*"))
             {
+                //if it's clossing directory command
+                if(e.CommandID == 1) {
+                    CloseHandle(sender, new DirectoryCloseEventArgs(m_path, "closing directory"));
+                }
                 string msg = m_controller.ExecuteCommand(e.CommandID, e.Args, out result);
 
                 //here we need to check the result and send to the logger
@@ -56,10 +61,12 @@ namespace ImageService.Controller.Handlers
         {
             string[] fullPath = { e.FullPath }; 
             string pathExtension = Path.GetExtension(e.FullPath);
+            m_logging.Log("OnCreated" + m_path, MessageTypeEnum.INFO);
             if (pathExtension.Equals(".jpg") || pathExtension.Equals(".png") || pathExtension.Equals(".gif") || pathExtension.Equals(".bmp")) {
                 CommandRecievedEventArgs commandRecievedEventArgs = new CommandRecievedEventArgs(0,fullPath , m_path);
                 OnCommandRecieved(this, commandRecievedEventArgs);
             }
+            m_logging.Log("Created new File" + e.FullPath ,MessageTypeEnum.INFO);
         }
 
         public void StartHandleDirectory(string dirPath)
@@ -69,7 +76,19 @@ namespace ImageService.Controller.Handlers
             m_dirWatcher.Created += new FileSystemEventHandler(OnCreated);
             // begin watching .
             m_dirWatcher.EnableRaisingEvents = true;
+            this.m_logging.Log("Start to handle directory"+dirPath,MessageTypeEnum.INFO);
         }
+        public void CloseHandle(object sender, DirectoryCloseEventArgs closeEventArgs)
+        {
+            try
+            {
+                m_dirWatcher.EnableRaisingEvents = false;
+                // we invoke to the server ' that it will know this directory was clossed: CommandRecieved -= this.OnCommandRecieved;
+                DirectoryClose?.Invoke(sender, closeEventArgs);
+            } catch (Exception e){
+                this.m_logging.Log("couldnt close handler" + this.m_path + " " + e.Message, MessageTypeEnum.FAIL);
 
+            }
+        }
     }
 }
