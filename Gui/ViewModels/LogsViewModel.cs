c0bebf1d0fd9303d702ccacae7b6ad177530214b
Omer Dekel <omerdekel92@gmail.com>
2018-05-24 23:05:52 +0300

    loging model

--- Gui/ViewModels/LogsViewModel.cs
+++ Gui/ViewModels/LogsViewModel.cs
@@ -3,27 +3,31 @@ using ImageService.Logging.Modal;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Gui.ViewModels
 {
-    class LogsViewModel
+    class LogsViewModel : INotifyPropertyChanged
     {
-        private LogsModel model ;
+        private LogsModel model;
         //constructor
         public LogsViewModel()
         {
             model = new LogsModel();
         }
-        public ObservableCollection<MessageRecievedEventArgs> Logs {
+        public ObservableCollection<MessageRecievedEventArgs> Logs
+        {
             get
             {
                 return model.LogsCollection;
             }
-            }
+        }
 
         public LogsModel Model { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
