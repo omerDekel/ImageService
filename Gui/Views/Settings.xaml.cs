7a7c06a2cb7ff4bc9858f01701e46001ca7ae0d0
Omer Dekel <omerdekel92@gmail.com>
2018-05-27 14:01:49 +0300

    remove column span from settings xaml

---
+++ Gui/Views/Settings.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Gui.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gui.Views
+{
+    /// <summary>
+    /// Interaction logic for settings.xaml
+    /// </summary>
+    public partial class Settings : UserControl
+    {
+        public ObservableCollection<string> DirectoryHandlers { get; set; }
+        public Settings()
+        {
+
+            InitializeComponent();
+            DataContext = new SettingsViewModel();
+        }
+
+    }
+}
