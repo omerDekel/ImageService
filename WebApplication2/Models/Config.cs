91902159ef6eea023f895dcc3b9acbf07afc2de4
Omer Dekel <omerdekel92@gmail.com>
2018-06-07 20:40:39 +0300

    Start of web Application

---
+++ WebApplication2/Models/Config.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Config
+    {
+        /*    <add key="Handler" value="C:\Users\Omer Dekel\Desktop\dor;C:\Users\Omer Dekel\Pictures\pict"/>
+    <add key="OutputDir" value="C:\Users\Omer Dekel\Desktop\IM"/>
+    <add key="SourceName" value="ImageServiceSource"/>
+    <add key="LogName" value="ImageServiceLog"/>
+    <add key="ThumbnailSize" value="120"/>
+*/
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "DirectoryHandlers")]
+        public List<string> DirectoryHandlers { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "OutputDirectory")]
+        public string OutputDirectory { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "SourceName")]
+        public string SourceName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "LogName")]
+        public string LogName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "ThumbnailSize")]
+        public string ThumbnailSize { get; set; }
+
+    }
+}
