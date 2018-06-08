e99f15373c80300ed601526b2086e50da062e97f
Omer Dekel <omerdekel92@gmail.com>
2018-06-08 13:36:30 +0300

    adding client to web application

---
+++ WebApplication2/Comunication/Client.cs
@@ -0,0 +1,129 @@
+﻿using ImageService.Controller;
+using ImageService.Infrastructure.Enums;
+using ImageService.Modal;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Comunication
+{
+    class Client : IClient
+    {
+        private static Mutex mutex;
+        private TcpClient tcpClient;
+        private TcpListener listener;
+        private bool stop;
+        public event EventHandler<CommandRecievedEventArgs> CommandRecieved;          // The event that notifies about a new Command being recieved
+        private static Client instance;
+        /// <summary>
+        /// constructor .
+        /// </summary>
+        /// 
+        public Client()
+        {
+            ConnectServer();
+        }
+        /// <summary>
+        /// instance for creating singleton of the client.
+        /// </summary>
+        public static Client Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Client();
+                }
+                return instance;
+            }
+        }
+        public bool Stop
+        {
+            get
+            {
+                return this.stop;
+            }
+        }
+
+        /// <summary>
+        /// task that listenning to the server to get command .
+        /// </summary>
+        public void CommandFromServer()
+        {
+            string commandStr;
+            new Task(() =>
+            {
+                Console.WriteLine("listening to the server ");
+                while (!stop)
+                {
+                    try
+                    {
+                        NetworkStream stream = tcpClient.GetStream();
+                        BinaryReader reader = new BinaryReader(stream);
+                        commandStr = reader.ReadString();
+                        Console.WriteLine($"Recieve {commandStr} from Server");
+                        CommandRecievedEventArgs responseObj = JsonConvert.DeserializeObject<CommandRecievedEventArgs>(commandStr);
+                        CommandRecieved?.Invoke(this, responseObj);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }).Start();
+        }
+        /// <summary>
+        /// sending command to the server .
+        /// </summary>
+        /// <param name="e">arguments for the recieved command</param>
+        public void CommandToServer(CommandRecievedEventArgs e)
+        {
+            Console.WriteLine("Send command to server by a diffrent task");
+
+            new Task(() =>
+            {
+                try
+                {
+                    NetworkStream stream = tcpClient.GetStream();
+                    BinaryWriter writer = new BinaryWriter(stream);
+                    string jStr = JsonConvert.SerializeObject(e);
+                    Console.WriteLine(jStr);
+                    Console.WriteLine($"Send {jStr} to Server");
+                    writer.Write(jStr);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    Console.WriteLine(exception.Source);
+                }
+            }).Start();
+        }
+        /// <summary>
+        /// connecting the client to server .
+        /// </summary>
+        public void ConnectServer()
+        {
+            try
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+                this.tcpClient = new TcpClient();
+                this.tcpClient.Connect(ep);
+                Console.WriteLine("You are connected");
+                stop = false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                stop = true;
+            }
+        }
+
+    }
+}
--- WebApplication2/Models/Config.cs
+++ WebApplication2/Models/Config.cs
@@ -2,25 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using WebApplication2.Comunication;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.ObjectModel;
+using ImageService.Infrastructure;
+using ImageService.Modal;
+using ImageService.Infrastructure.Enums;
 
 namespace WebApplication2.Models
 {
     public class Config
     {
-        /*    <add key="Handler" value="C:\Users\Omer Dekel\Desktop\dor;C:\Users\Omer Dekel\Pictures\pict"/>
-    <add key="OutputDir" value="C:\Users\Omer Dekel\Desktop\IM"/>
-    <add key="SourceName" value="ImageServiceSource"/>
-    <add key="LogName" value="ImageServiceLog"/>
-    <add key="ThumbnailSize" value="120"/>
-*/
+        
+        /*private String outputDirectory;
+        private String sourceName;
+        private String logName;
+        private String thumbnailSize;*/
+        // the client we comunicate through it with the server .
+       private IClient clientGui;
+        /// <summary>
+        /// Constructor .
+        /// </summary>
+        public Config()
+        {
+            /*clientGui = Client.Instance;
+            clientGui.CommandRecieved += OnCommandRecieved;
+            clientGui.CommandFromServer();*/
+            //taking the config arguments by the GetConfigCommand
+            DirectoryHandlers = new ObservableCollection<string>();
+            /*string[] arguments = new string[5];
+            CommandRecievedEventArgs getConfigCommand = new CommandRecievedEventArgs((int)CommandEnum.GetConfigCommand, arguments, "");
+            clientGui.CommandToServer(getConfigCommand);*/
+        }
+        public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
+        {
+            /*try
+            {
+                //if it's command of get config .
+                if (e.CommandID == (int)CommandEnum.GetConfigCommand)
+                {
+                    OutputDirectory = e.Args[0];
+                    Console.Write("OUTPUT DIRECTORY IS:");
+                    Console.WriteLine(e.Args[0]);
+                    SourceName = e.Args[1];
+                    LogName = e.Args[2];
+                    ThumbnailSize = e.Args[3];
+                    string[] directoryArray = e.Args[4].Split(';');
+                    for (int i = 0; i < directoryArray.Length; i++)
+                    {
+                        string dir = directoryArray[i];
+                        Console.WriteLine("dir name" + dir);
+
+
+                        Console.WriteLine("dir was added " + dir);
+                       // App.Current.Dispatcher.Invoke((Action)delegate
+                       // {
+                            DirectoryHandlers.Add(dir);
+
+                       // });
+                    }
+                }
+                //if it's event of close directory command .
+                else if (e.CommandID == (int)CommandEnum.CloseCommand)
+                {
+                    if (e.RequestDirPath != null && DirectoryHandlers != null && DirectoryHandlers.Contains(e.RequestDirPath))
+                    {
+                        //App.Current.Dispatcher.Invoke((Action)delegate
+                       //// {
+                            this.DirectoryHandlers.Remove(e.RequestDirPath);
+
+                      //  });
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }*/
+        }
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "DirectoryHandlers")]
-        public List<string> DirectoryHandlers { get; set; }
+        public ObservableCollection<string> DirectoryHandlers { get; set; }
 
 
         [Required]
