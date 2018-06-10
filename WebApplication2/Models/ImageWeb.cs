7ed7ade091f73a01965c602326b4d1968855c7cc
Omer Dekel <omerdekel92@gmail.com>
2018-06-11 00:14:29 +0300

    Start reading students

--- WebApplication2/Models/ImageWeb.cs
+++ WebApplication2/Models/ImageWeb.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using WebApplication2.Comunication;
 
 namespace WebApplication2.Models
 {
     public class ImageWeb
     {
+        private IClient m_client;
+        //public IClient M_client { get; set; }
+
+        public ImageWeb()
+        {
+            // connecting to the server Image service .
+            m_client = Client.Instance;
+            // if the connection didn't stop then there's a connection .
+            if (m_client.Stop == true)
+            {
+                Status = "Disconnected";
+            } else
+            {
+                Status = "Connected";
+            }
+            Students = new List<Student>();
+
+        }
+
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "PicturesNumber")]
@@ -15,8 +35,13 @@ namespace WebApplication2.Models
 
         [Required]
         [DataType(DataType.Text)]
-        [Display(Name = "IsConnected")]
-        public bool IsConnected { get; set; }
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Students")]
+        public List<Student> Students { get; set; }
 
     }
 }
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+    }
+}
