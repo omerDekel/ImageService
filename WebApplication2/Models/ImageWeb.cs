b79f1d06a4980face4e8b22e1fe01ba6565eb2d4
Yifat <yifatyank@gmail.com>
2018-06-10 21:59:06 +0300

    Revert "now we can see image web home"
    This reverts commit bef8d42c5624fbb0b3783e4550a9cab68881f829.

--- WebApplication2/Models/ImageWeb.cs
+++ WebApplication2/Models/ImageWeb.cs
@@ -3,23 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using WebApplication2.Comunication;
 
 namespace WebApplication2.Models
 {
     public class ImageWeb
     {
-
-        private IClient m_client;
-        //public IClient M_client { get; set; }
-
-        public ImageWeb()
-        {
-            // connecting to the server Image service .
-            m_client = Client.Instance;
-            // if the connection didn't stop then there's a connection .
-            IsConnected = !m_client.Stop;
-        }
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "PicturesNumber")]
