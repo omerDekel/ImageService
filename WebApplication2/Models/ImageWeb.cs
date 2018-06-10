4608c80f9f55cf1c7e8f524720933e739116bec4
Yifat <yifatyank@gmail.com>
2018-06-10 22:09:58 +0300

    Revert "now we can see image web home"
    This reverts commit bef8d42c5624fbb0b3783e4550a9cab68881f829.

---
+++ WebApplication2/Models/ImageWeb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ImageWeb
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "PicturesNumber")]
+        public int PicturesNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "IsConnected")]
+        public bool IsConnected { get; set; }
+
+    }
+}
