0ebddae4b6cd10ff89404c4789ad72537b0786e2
Omer Dekel <omerdekel92@gmail.com>
2018-06-11 14:23:41 +0300

    creating photos page

--- WebApplication2/Controllers/PhotosController.cs
+++ WebApplication2/Controllers/PhotosController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class PhotosController : Controller
     {
+        static Photos PhotosModel = new Photos();
         // GET: Photos
-        public ActionResult Index()
+        public ActionResult PhotosView()
         {
-            return View();
+            return View(PhotosModel);
         }
     }
 }
